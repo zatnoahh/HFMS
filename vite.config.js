957aee8a28e4b446a4c1f1b40af47383f247dc4a
Izzat Syahmi <syahmiizzat550@gmail.com>
2025-03-02 15:24:59 +0800

    Update Vite configuration to include CSS input and enable build manifest

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
+        laravel([
+            'resources/css/app.css',
+            'resources/sass/app.scss',
+            'resources/js/app.js',
+        ]),
     ],
+    build: {
+        manifest: true,
+    },
 });
